88884af2b10a2e02294cf30bf1260fbe3a5e60c0
AlexanderSinn <alexander.sinn@studium.uni-hamburg.de>
2020-05-18 16:56:33 +0000

    erster Versuch

--- likelihood.cc
+++ likelihood.cc
@@ -1,37 +1,66 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
-double poisson(double mu, int k) {
+long double poisson(double mu, int k) {
   using namespace std;
-  double P = pow(mu, k) * exp(-mu) / tgamma(k + 1);
+  long double P = pow(mu, k) * exp(-mu) / tgamma(k + 1);
   return P;
 }
 
-double prob(std::vector<int> daten, double mu) {
+long double prob(std::vector<int> daten, double mu) {
   using namespace std;
   long double P = 1;
-  for(int k : daten) { 
-    P *= poisson(mu , k);
-   }
-   return P;
+  for (int k : daten) {
+    P *= poisson(mu, k);
   }
+  return P;
+}
+
+long double prob2(std::vector<int> daten) {
+  using namespace std;
+  long double P = 1;
+  for (int k : daten) {
+    P *= poisson(k, k);
+  }
+  return P;
+}
 
 int main() {
   using namespace std;
   ifstream fin("datensumme.txt");
+  ofstream like("likelihood.txt");
+  ofstream nll("nll.txt");
+  ofstream deltanll("deltanll.txt");
 
-    vector<int> daten(0);
-
+  vector<int> daten(0);
   int n_i;
-  for(int i = 0 ; i < 234 ; ++i) {
+
+  for (int i = 0; i < 234; ++i) {
     fin >> n_i;
     daten.push_back(n_i);
-    }
+  }
 
- fin.close();
+  cout << "L(3.11538) = " << prob(daten, 3.11538) << endl;
 
-cout <<  prob(daten , 3.11538 )<< endl;
+  for (double i = 0; i <= 6; i += 0.1) {
+    like << i << " " << prob(daten, i) << endl;
+    nll << i << " " << -2 * log(prob(daten, i)) << endl;
+    deltanll << i << " " << -2 * log(prob(daten, i)) + 2 * log(prob(daten, 3.11538)) << endl;
+  }
 
+  long double delta = prob(daten, 3.11538) / prob2(daten);
+
+  cout << "-2*ln(\u039B) = " << -2 * log(delta) << endl;
+  cout << "z = " << (-2 * log(delta) - 233) / sqrt(2 * 233) << endl;
+
+  // bester Wert: 3.11538461
+  // Intervall (3.002-3,232) mit µ<1
+  // bestes µ=3,115 +0,117 -0,113
+
+  fin.close();
+  like.close();
+  nll.close();
+  deltanll.close();
 }
